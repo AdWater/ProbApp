d5913ddf3866a89cf3d6a0ba6ccff676bef6bfe2
David McInerney <a1065639@adelaide.edu.au>
2025-11-03 11:21:19 +1030

    fixed minor bug with inputs to calc_ClimDaily_dayOfYearWindow_seamless()

--- R/calc_metrics.R
+++ R/calc_metrics.R
@@ -67,7 +67,7 @@ abs_bias_prob =  function(obs,pred){
 calc_metrics = function(data,pred.reps,opt){
   reliability = PQQ_alpha(obs=data[[opt$obs]],pred.reps=pred.reps,perturb=T)
   pred.reps.base = calc_ClimDaily_dayOfYearWindow_seamless(QobsCal=data[[opt$obs]],
-                                                           datesCal=as.Date(data[[opt$dateName]],format=opt$dateFormat))
+                                                           datesCal=as.Date(data[[opt$date]]))
   sharpness = sharpness(pred.reps=pred.reps,pred.reps.base=pred.reps.base)
   bias = abs_bias_prob(obs=data[[opt$obs]],pred=pred.reps)
 
